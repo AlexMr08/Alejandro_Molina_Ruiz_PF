7d118a23c7bff2d2839441d9f81e57df1dbb7764
Alex <alex.molinaruiz@gmail.com>
2022-02-01 18:00:13 +0100

    cambios en el layout de añadir carta

--- app/src/main/java/com/example/practica_final/ControlSP.kt
+++ app/src/main/java/com/example/practica_final/ControlSP.kt
@@ -3,26 +3,53 @@ package com.example.practica_final
 import android.content.Context
 import android.content.SharedPreferences
 
-class ControlSP(c : Context, sp : SharedPreferences) {
-        val contexto = c
-        val sp = sp
-        var tipo = sp.getInt(c.getString(R.string.sp_tipo),1)
-        var id = sp.getString(c.getString(R.string.sp_usuario),"")
+class ControlSP(c: Context, sp: SharedPreferences) {
+    val contexto = c
+    val ID = "com.example.practica_final"
+    val shared_name = "${ID}_sp"
+    val sp = contexto.getSharedPreferences(shared_name, 0)
 
-    fun cambiarSPPerfil(idn:String, tip:Int){
-        with(sp.edit()){
-            putString(contexto.getString(R.string.sp_usuario),idn)
-            putInt(contexto.getString(R.string.sp_tipo),tip)
+    //Tipo de usuario
+    var tipo: Int
+        get() = sp.getInt(
+            contexto.getString(R.string.sp_tipo),
+            contexto.resources.getInteger(R.integer.sp_tipo_def)
+        ) ?: contexto.resources.getInteger(R.integer.sp_tipo_def)
+        set(value) = sp.edit().putInt(contexto.getString(R.string.sp_tipo), value).apply()
+
+    //Id del usuario
+    var id: String
+        get() = sp.getString(
+            contexto.getString(R.string.sp_usuario),
+            ""
+        ) ?: ""
+        set(value) = sp.edit().putString(
+            contexto.getString(R.string.sp_usuario), value).apply()
+
+    //Ultima comprobacion API monedas
+    var ultimaComp: String
+        get() = sp.getString(
+            contexto.getString(R.string.sp_ultimaComp),
+            contexto.getString(R.string.sp_ultimaComp_def)
+        ) ?: contexto.getString(R.string.sp_ultimaComp_def)
+
+        set(value) = sp.edit().putString(
+            contexto.getString(R.string.sp_ultimaComp),
+            value).apply()
+
+    var eur_usd: Float
+        get() = sp.getString(
+            contexto.getString(R.string.sp_eur_usd),
+            contexto.resources.getString(R.string.sp_eur_usd_def)
+        )?.toFloat() ?: contexto.resources.getString(R.string.sp_eur_usd_def).toFloat()
+
+        set(value) = sp.edit().putString(contexto.getString(R.string.sp_tipo), value.toString()).apply()
+
+    fun borrarSPPerfil() {
+        with(sp.edit()) {
+            putString(contexto.getString(R.string.sp_usuario), "")
+            putInt(contexto.getString(R.string.sp_tipo), 1)
             commit()
-        }
-    }
-    fun borrarSPPerfil(){
-        with(sp.edit()){
-            with(sp.edit()){
-                putString(contexto.getString(R.string.sp_usuario),"")
-                putInt(contexto.getString(R.string.sp_tipo),1)
-                commit()
-            }
         }
     }
 }
